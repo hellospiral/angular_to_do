46246b9ed11555f3406bd570f79d46bf002c27ab
Matt Carlson <matthewthomascarlson@gmail.com>
2016-08-08 14:13:45 -0700

    allow you to create new tasks and edit old ones

--- app/task-list.component.ts
+++ app/task-list.component.ts
@@ -1,19 +1,24 @@
 import { Component, EventEmitter } from 'angular2/core';
 import { TaskComponent } from './task.component';
 import { Task } from './task.model';
+import { EditTaskDetailsComponent } from './edit-task-details.component';
+import { NewTaskComponent } from './new-task.component';
 
 // Child Component: TaskList
 @Component({
   selector: 'task-list',
   inputs: ['taskList'],
   outputs: ['onTaskSelect'], // 2. Decide the name of our event emitter
-  directives: [TaskComponent],
+  directives: [TaskComponent, EditTaskDetailsComponent, NewTaskComponent],
   template: `
   <task-display *ngFor="#currentTask of taskList"
     (click)="taskClicked(currentTask)"
     [class.selected]="currentTask === selectedTask"
     [task]="currentTask">
   </task-display>
+  <edit-task-details *ngIf="selectedTask" [task]="selectedTask">
+  </edit-task-details>
+  <new-task (onSubmitNewTask)="createTask($event)"></new-task>
   ` // 5. The timing(click); trigger the correct 'vehicles' to go across the 'bridge'; use the built-in click event emitter to trigger a method called taskClicked
 })
 export class TaskListComponent {
@@ -27,5 +32,10 @@ export class TaskListComponent {
     console.log('child', clickedTask);
     this.selectedTask = clickedTask;
     this.onTaskSelect.emit(clickedTask); // 6. Use the emit method from inside the correct method to tell the vehicle (our task) to go. That emit method belongs to our onTaskSelect bridge.
+  }
+  createTask(description: string): void {
+    this.taskList.push(
+      new Task(description, this.taskList.length)
+    );
   }
 }
